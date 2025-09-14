3f1e7432eb048926df587b979c90a8e0e6d4c5ee
Pascal Leconte <pascal.leconte@conseilgouz.com>
2025-09-14 16:08:19 +0200

    1.0.3

---
+++ script.php
@@ -0,0 +1,183 @@
+<?php
+// No direct access to this file
+defined('_JEXEC') or die;
+use Joomla\CMS\Factory;
+use Joomla\CMS\Version;
+use Joomla\Database\DatabaseInterface;
+use Joomla\Filesystem\File;
+use Joomla\Filesystem\Folder;
+
+class plgsystemconseilgouzInstallerScript
+{
+    private $min_joomla_version      = '4.0.0';
+    private $min_php_version         = '8.0';
+    private $installerName  = 'conseilgouz';
+    private $extname        = 'conseilgouz';
+    private $newlib_version = '';
+    private $dir;
+    private $lang;
+
+    public function __construct()
+    {
+        $this->dir = __DIR__;
+        $this->lang = Factory::getApplication()->getLanguage();
+        $this->lang->load($this->extname);
+    }
+
+    /**
+     * method to run before an install/update/uninstall method
+     *
+     * @return void
+     */
+    public function preflight($type, $parent)
+    {
+        if (! $this->passMinimumJoomlaVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+
+        if (! $this->passMinimumPHPVersion()) {
+            $this->uninstallInstaller();
+            return false;
+        }
+    }
+
+    /**
+     * method to run after an install/update/uninstall method
+     *
+     * @return void
+     */
+    public function postflight($type, $parent)
+    {
+        if (($type == 'install') || ($type == 'update')) { // remove obsolete dir/files
+            if (!$this->checkLibrary('conseilgouz')) { // need library installation
+                $ret = $this->installPackage('lib_conseilgouz');
+                if ($ret) {
+                    Factory::getApplication()->enqueueMessage('ConseilGouz Library ' . $this->newlib_version . ' installed', 'notice');
+                }
+            }
+            $this->postinstall_cleanup();
+        }
+        return true;
+    }
+
+    private function postinstall_cleanup()
+    {
+        if (!$this->checkLibrary('conseilgouz')) { // need library installation
+            $ret = $this->installPackage('lib_conseilgouz');
+            if ($ret) {
+                Factory::getApplication()->enqueueMessage('ConseilGouz Library ' . $this->newlib_version . ' installed', 'notice');
+            }
+        }
+        // Uninstall this installer
+        $this->uninstallInstaller();
+    }
+    private function checkLibrary($library)
+    {
+        $file = $this->dir.'/lib_conseilgouz/conseilgouz.xml';
+        if (!is_file($file)) {// library not installed
+            return false;
+        }
+        $xml = simplexml_load_file($file);
+        $this->newlib_version = $xml->version;
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $conditions = array(
+             $db->qn('type') . ' = ' . $db->q('library'),
+             $db->qn('element') . ' = ' . $db->quote($library)
+            );
+        $query = $db->getQuery(true)
+                ->select('manifest_cache')
+                ->from($db->quoteName('#__extensions'))
+                ->where($conditions);
+        $db->setQuery($query);
+        $manif = $db->loadObject();
+        if ($manif) {
+            $manifest = json_decode($manif->manifest_cache);
+            if ($manifest->version >= $this->newlib_version) { // compare versions
+                return true; // need library
+            }
+        }
+        return false; // need library
+    }
+    private function installPackage($package)
+    {
+        $tmpInstaller = new Joomla\CMS\Installer\Installer();
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $tmpInstaller->setDatabase($db);
+        $installed = $tmpInstaller->install($this->dir . '/' . $package);
+        return $installed;
+    }
+
+    // Check if Joomla version passes minimum requirement
+    private function passMinimumJoomlaVersion()
+    {
+        $j = new Version();
+        $version = $j->getShortVersion();
+        if (version_compare($version, $this->min_joomla_version, '<')) {
+            if ($j->getHelpVersion() == '.310') {
+                Factory::getApplication()->enqueueMessage(
+                    'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong><br><br>Please download <a href="https://github.com/conseilgouz/AutoReadMore-J4/releases/download/5.1.5/AutoReadMore-J4-5.1.5.zip" download="AutoReadMore-J4-5.1.5.zip">AutoreadMore version 5.1.5</a> for Joomla! 3.10.<br><br>',
+                    'error'
+                );
+            } else {
+                Factory::getApplication()->enqueueMessage(
+                    'Incompatible Joomla version : found <strong>' . $version . '</strong>, Minimum : <strong>' . $this->min_joomla_version . '</strong>',
+                    'error'
+                );
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    // Check if PHP version passes minimum requirement
+    private function passMinimumPHPVersion()
+    {
+
+        if (version_compare(PHP_VERSION, $this->min_php_version, '<')) {
+            Factory::getApplication()->enqueueMessage(
+                'Incompatible PHP version : found  <strong>' . PHP_VERSION . '</strong>, Minimum <strong>' . $this->min_php_version . '</strong>',
+                'error'
+            );
+            return false;
+        }
+
+        return true;
+    }
+    private function uninstallInstaller()
+    {
+        if (! is_dir(JPATH_PLUGINS . '/system/' . $this->installerName)) {
+            return;
+        }
+        $this->delete([
+            JPATH_PLUGINS . '/system/' . $this->installerName . '/language',
+            JPATH_PLUGINS . '/system/' . $this->installerName,
+        ]);
+        $db = Factory::getContainer()->get(DatabaseInterface::class);
+        $query = $db->getQuery(true)
+            ->delete('#__extensions')
+            ->where($db->quoteName('element') . ' = ' . $db->quote($this->installerName))
+            ->where($db->quoteName('folder') . ' = ' . $db->quote('system'))
+            ->where($db->quoteName('type') . ' = ' . $db->quote('plugin'));
+        $db->setQuery($query);
+        $db->execute();
+        $cache = Factory::getContainer()->get(Joomla\CMS\Cache\CacheControllerFactoryInterface::class)->createCacheController();
+        $cache->clean('_system');
+    }
+    public function delete($files = [])
+    {
+        foreach ($files as $file) {
+            if (is_dir($file)) {
+                Folder::delete($file);
+            }
+
+            if (is_file($file)) {
+                File::delete($file);
+            }
+        }
+    }
+
+}
+?>
+
